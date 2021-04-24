115e64a1350d5113ea2ba1e121e421730b080213
rgdopazo18 <rgdopazo18@esei.uvigo.es>
2021-04-25 00:03:12 +0200

    Creada la primera version de data.json

--- handlers/main.py
+++ handlers/main.py
@@ -7,12 +7,25 @@ pilotos ya existentes. Todos estos datos son guardados en data.json
 
 import webapp2
 from webapp2_extras import jinja2
+import json
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        # read file
+        with open('data.json', 'r') as f:
+            data = f.read()
+        root = json.loads(data)
+        sports = root["sports"]
+        first_sport = sports[0]
+        drivers = list(first_sport["drivers"])
+
+        susts = {
+            "drivers": drivers
+        }
+
         jinja = jinja2.get_jinja2(app=self.app)
-        self.response.write(jinja.render_template("main.html"))
+        self.response.write(jinja.render_template("main.html", **susts))
 
     def post(self):
         jinja = jinja2.get_jinja2(app=self.app)
