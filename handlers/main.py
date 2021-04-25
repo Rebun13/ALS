150b709116721f91d78bd07b51e2dc0898749c7a
rgdopazo18 <rgdopazo18@esei.uvigo.es>
2021-04-25 16:37:38 +0200

    Eliminados otros deportes y corregida la expresion regular para nombre

--- handlers/main.py
+++ handlers/main.py
@@ -14,11 +14,8 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         # read file
         with open('data.json', 'r') as f:
-            data = f.read()
-        root = json.loads(data)
-        sports = root["sports"]
-        first_sport = sports[0]
-        drivers = list(first_sport["drivers"])
+            data = json.load(f)
+        drivers = list(data["drivers"])
 
         susts = {
             "drivers": drivers
@@ -28,8 +25,28 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write(jinja.render_template("main.html", **susts))
 
     def post(self):
-        jinja = jinja2.get_jinja2(app=self.app)
-        self.response.write(jinja.render_template("main.html"))
+        post_type = self.request.get('form', 'ERROR')
+        if post_type == "insert":
+            nombre = self.request.get('name', 'ERROR')
+            id_piloto = self.request.get('id', 'ERROR')
+            puntos = self.request.get('score', 'ERROR')
+            with open('data.json', 'a') as f:
+                data = json.load(f)
+                data["drivers"].append({"id": id_piloto, "name": nombre, "score": puntos})
+        elif post_type == "modify":
+            id_piloto = self.request.get('id', 'ERROR')
+            puntos = self.request.get('score', 'ERROR')
+            with open('data.json', 'r') as f:
+                data = json.load(f)
+                for d in data:
+                    if d["id"] == id_piloto:
+                        driver = d
+                        break
+            driver["score"] = puntos
+            with open('data.json', 'w') as f:
+                json.dump(driver, f)
+        else:
+            pass
 
 
 app = webapp2.WSGIApplication([
